9f40e0bfe51e2cc829cae63916dd47139e613238
Shawn <coolrainerseth@gmail.com>
2018-09-28 00:29:56 +0800

    learning

---
+++ test04_classification.py
@@ -0,0 +1,16 @@
+"""  
+    author: Shawn
+    time  : 9/28/18 12:13 AM
+    desc  :
+    update: Shawn 9/28/18 12:13 AM      
+"""
+
+# TensorFlow and tf.keras
+import tensorflow as tf
+from tensorflow import keras
+
+# Helper libraries
+import numpy as np
+import matplotlib.pyplot as plt
+
+print(tf.__version__)
