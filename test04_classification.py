0b3e41b57d8c65d141e2401c1a971b0da22a80ad
Shawn <wenchunyu@xindedata.com>
2018-09-28 18:51:35 +0800

    learning classification

--- test04_classification.py
+++ test04_classification.py
@@ -1,7 +1,7 @@
 """  
     author: Shawn
     time  : 9/28/18 12:13 AM
-    desc  :
+    desc  : classification of clothing
     update: Shawn 9/28/18 12:13 AM      
 """
 
@@ -13,4 +13,140 @@ from tensorflow import keras
 import numpy as np
 import matplotlib.pyplot as plt
 
-print(tf.__version__)
+fashion_mnist = keras.datasets.fashion_mnist
+
+# [(60000, 28, 28), (60000,)], [(10000, 28, 28), (10000,)]
+(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
+               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
+
+def print_basic_info():
+    print(train_images.shape)
+    print(train_labels.shape)
+    # print(len(test_labels))
+    # print(train_images[0])
+    # print(train_labels[0])
+    # print(tf.__version__)
+
+
+def show_image():
+    plt.figure()
+    plt.imshow(test_images[0])
+    plt.colorbar()
+    plt.grid(False)
+    print('show image')
+    plt.show()
+    pass
+
+
+train_images = train_images / 255.0
+test_images = test_images / 255.0
+
+
+def show_image2():
+    plt.figure(figsize=(10, 10))
+    for i in range(25):
+        plt.subplot(5, 5, i + 1)
+        plt.xticks([])
+        plt.yticks([])
+        plt.grid(False)
+        plt.imshow(train_images[i], cmap=plt.cm.binary)
+        plt.xlabel(class_names[train_labels[i]])
+
+
+def start_training():
+    # three layers
+    model = keras.Sequential([
+        keras.layers.Flatten(input_shape=(28, 28)),
+        keras.layers.Dense(128, activation=tf.nn.relu),
+        keras.layers.Dense(10, activation=tf.nn.softmax)
+    ])
+
+    model.compile(optimizer=tf.train.AdamOptimizer(),
+                  loss='sparse_categorical_crossentropy',
+                  metrics=['accuracy'])
+
+    model.fit(train_images, train_labels, epochs=5)
+
+    test_loss, test_acc = model.evaluate(test_images, test_labels)
+    print('Test accuracy:', test_acc)
+
+    # predict single image
+    # Add the image to a batch where it's the only member.
+    img = (np.expand_dims(test_images[9], 0))
+    prediction = model.predict(img)
+    print(np.argmax(prediction))
+    print(test_labels[9])
+
+    # predict multi images
+    # predictions = model.predict(test_images)
+    # predict1(predictions)
+    # predict2(predictions)
+
+
+def predict2(predictions):
+    # Plot the first X test images, their predicted label, and the true label
+    # Color correct predictions in blue, incorrect predictions in red
+    num_rows = 5
+    num_cols = 3
+    num_images = num_rows * num_cols
+    plt.figure(figsize=(2 * 2 * num_cols, 2 * num_rows))
+    for i in range(num_images):
+        plt.subplot(num_rows, 2 * num_cols, 2 * i + 1)
+        plot_image(i, predictions, test_labels, test_images)
+        plt.subplot(num_rows, 2 * num_cols, 2 * i + 2)
+        plot_value_array(i, predictions, test_labels)
+
+
+def predict1(predictions):
+    print(np.argmax(predictions[0]))
+    i = 0
+    plt.figure(figsize=(6, 3))
+    plt.subplot(1, 2, 1)
+    plot_image(i, predictions, test_labels, test_images)
+    plt.subplot(1, 2, 2)
+    plot_value_array(i, predictions, test_labels)
+
+
+def plot_image(i, predictions_array, true_label, img):
+    predictions_array, true_label, img = predictions_array[i], true_label[i], img[i]
+    plt.grid(False)
+    plt.xticks([])
+    plt.yticks([])
+
+    plt.imshow(img, cmap=plt.cm.binary)
+
+    predicted_label = np.argmax(predictions_array)
+    if predicted_label == true_label:
+        color = 'blue'
+    else:
+        color = 'red'
+
+    plt.xlabel("{} {:2.0f}% ({})".format(class_names[predicted_label],
+                                         100 * np.max(predictions_array),
+                                         class_names[true_label]),
+               color=color)
+    # plt.show()
+
+
+def plot_value_array(i, predictions_array, true_label):
+    predictions_array, true_label = predictions_array[i], true_label[i]
+    plt.grid(False)
+    plt.xticks([])
+    plt.yticks([])
+    thisplot = plt.bar(range(10), predictions_array, color="#777777")
+    plt.ylim([0, 1])
+    predicted_label = np.argmax(predictions_array)
+
+    thisplot[predicted_label].set_color('red')
+    thisplot[true_label].set_color('blue')
+
+
+if __name__ == '__main__':
+    # printBasicInfo()
+    # show_image()
+    start_training()
+    plt.show()
+    pass
---
+++ test99_tmp.py
@@ -0,0 +1,12 @@
+"""  
+    author: Shawn
+    time  : 18-9-28 下午5:32
+    desc  :
+    update: Shawn 18-9-28 下午5:32      
+"""
+
+import matplotlib.pyplot as plt
+from scipy import misc
+
+im = misc.imread("./zidan.jpg")
+plt.imshow(im, cmap=plt.cm.gray)
